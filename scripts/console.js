d0671f33c5d8eb46c4eb20d90a019b5a25a2a184
cathan-dev <cathan.dev@gmail.com>
2026-08-01 23:25:35 +0200

    nav update, arrow tab and enter control, overlay works, caret blinks, etc. good job me

--- scripts/console.js
+++ scripts/console.js
@@ -1,42 +1,182 @@
+// ---------- CONSTANTS ---------- //
+
 const navElements = {
     input: document.getElementById("nav-input"),
-    list: document.querySelectorAll(".nav-list"),
+    list: document.querySelector(".nav-list"),
     listItems: document.querySelectorAll(".nav-list-item"),
     listLinks: document.querySelectorAll(".nav-link"),
     listText: document.querySelectorAll(".nav-link-text"),
-    overlayText: document.querySelectorAll(".nav-overlay-text")
+    overlayText: document.querySelector(".nav-overlay-text"),
+    beforeCursor: document.querySelector(".nav-before-cursor"),
+    textCursor: document.querySelector(".nav-text-cursor"),
+    afterCursor: document.querySelector(".nav-after-cursor")
 }
 
 const navData = {
     inputText: "",
-    listPages: [...navElements.listLinks].map((link) => link.dataset.page)
+    listEligible: [...navElements.listLinks],
+    selectedIndex: 0,
+    errorFlag: false
 }
 
-function stateUpdate() {
+// ---------- INPUT FUNCTIONS ---------- //
+
+function inputHandler() {
+    navData.selectedIndex = 0
     navData.inputText = getInputText()
-    toggleLinks()
+    updateEligible()
+    render()
 }
 
 function getInputText() {
     return navElements.input.value
 }
 
-function toggleLinks() {
-    navElements.listItems.forEach((elem, index) => {
-        elem.classList.toggle("is-hidden", !matchLinkText(index))
+function updateEligible() {
+    navData.listEligible = [...navElements.listLinks].filter((link) => matchLinkText(link.dataset.page))
+}
+
+function matchLinkText(page) {
+    return page.toLowerCase().startsWith(navData.inputText.toLowerCase())
+}
+
+// ---------- KEYDOWN FUNCTIONS ---------- //
+
+function keydownHandler(e) {
+    switch (e.key) {
+        case "Enter":
+            e.preventDefault()
+            enterKeydownHandler()
+            render()
+            break
+        case "Tab":
+            e.preventDefault()
+            tabKeydownHandler()
+            render()
+            break
+        case "ArrowUp":
+            e.preventDefault()
+            upKeydownHandler()
+            render()
+            break
+        case "ArrowDown":
+            e.preventDefault()
+            downKeydownHandler()
+            render()
+            break
+        case "ArrowLeft":
+            e.preventDefault()
+            break
+        case "ArrowRight":
+            e.preventDefault()
+            break
+        case "Escape":
+            e.preventDefault()
+            navElements.input.blur()
+            render()
+            break
+    }
+}
+
+function enterKeydownHandler() {
+    if (navData.listEligible.length > 0) {
+        window.location.assign(navData.listEligible[navData.selectedIndex].href)
+    }
+}
+
+function tabKeydownHandler() {
+    if (navData.listEligible.length > 0) {
+        navElements.input.value = navData.listEligible[navData.selectedIndex].dataset.page
+        inputHandler()
+    }
+}
+
+function upKeydownHandler() {
+    if (navData.listEligible.length > 0) {
+        navData.selectedIndex -= 1
+        if (navData.selectedIndex < 0) {
+            navData.selectedIndex = navData.listEligible.length - 1
+        }
+    }
+}
+
+function downKeydownHandler() {
+    if (navData.listEligible.length > 0) {
+        navData.selectedIndex += 1
+        if (navData.selectedIndex >= navData.listEligible.length) {
+            navData.selectedIndex = 0
+        }
+    }
+}
+
+// ---------- CLICK FUNCTIONS ---------- //
+
+function clickHandler() {
+    caretReposition()
+}
+
+// ---------- FOCUS FUNCTIONS ---------- //
+
+function focusHandler() {
+    caretReposition()
+}
+
+// ---------- N/A FUNCTIONS ---------- //
+
+function caretReposition() {
+    navElements.input.setSelectionRange(navElements.input.value.length, navElements.input.value.length)
+}
+
+// ---------- RENDERING FUNCTIONS ---------- //
+
+function render() {
+    const selected = navData.listEligible[navData.selectedIndex]
+    renderItems(selected)
+    renderGhost(selected)
+}
+
+function renderItems(selected) {
+    navElements.listLinks.forEach((link) => {
+        link.closest("li").classList.toggle("is-hidden", !navData.listEligible.includes(link))
+        link.closest("li").classList.toggle("is-selected", link === selected)
     })
 }
 
-function matchLinkText(index) {
-    return navData.listPages[index].startsWith(navData.inputText)
+function renderGhost(selected) {
+    if (selected) {
+        navElements.beforeCursor.textContent = navData.inputText
+        if (navData.inputText.length >= selected.dataset.page.length) {
+            navElements.textCursor.textContent = "\u00A0"
+            navElements.afterCursor.textContent = ""
+        }
+        else {
+            navElements.textCursor.textContent = selected.dataset.page.charAt(navData.inputText.length)
+            navElements.afterCursor.textContent = selected.dataset.page.slice(navData.inputText.length + 1)
+        }
+    }
+    else {
+        navElements.beforeCursor.textContent = navData.inputText
+        navElements.textCursor.textContent = "\u00A0"
+        navElements.afterCursor.textContent = ""
+    }
 }
 
+// ---------- MAIN ---------- //
+
 function main() {
-    console.log(navElements)
-    console.log(navData)
-    navElements.input.addEventListener("input", () => {
-        stateUpdate()
-        render()
+    inputHandler()
+
+    navElements.input.addEventListener("input", (e) => {
+        inputHandler()
+    })
+    navElements.input.addEventListener("keydown", (e) => {
+        keydownHandler(e)
+    })
+    navElements.input.addEventListener("click", (e) => {
+        clickHandler()
+    })
+    navElements.input.addEventListener("focus", (e) => {
+        focusHandler()
     })
 }
 
