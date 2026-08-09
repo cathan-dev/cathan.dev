8cd0c2f062458b908fb4c451cf0a835072619b06
cathan-dev <cathan.dev@gmail.com>
2026-08-09 23:35:57 +0200

    deleted 2 scaffolded pages

--- src/data/nav-pages.js
+++ src/data/nav-pages.js
@@ -1,7 +1,5 @@
 export const pages = [
-    { href: '/help/', slug: 'help' },
     { href: '/', slug: 'home' },
-    { href: '/now/', slug: 'now' },
     { href: '/projects/', slug: 'projects' },
     { href: '/shelf/', slug: 'shelf' },
     { href: '/updates/', slug: 'updates' }
